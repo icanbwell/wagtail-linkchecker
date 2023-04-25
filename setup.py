6935ca03536f844c531caa2ec355d98ffabd5e9d
Nitin Bhardwaj <nitin.bhardwaj@icanbwell.com>
2023-04-25 12:29:20 +0530

    HP-3271: Updated version as per PEP 440

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-linkchecker',
-    version='0.6.0-bwell',
+    version='0.6.0+bwell',
     description="A tool to assist with finding broken links on your wagtail site.",
     long_description=readme,
     author='Neon Jungle',
